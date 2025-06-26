9d1aacb8e8b617b6413ba5285fb75a1802061711
Enrico Tassi <Enrico.Tassi@Inria.fr>
2025-06-26 12:16:03 +0200

    adapt to rocq-prover/rocq#20707

--- iris/algebra/lib/gmap_view.v
+++ iris/algebra/lib/gmap_view.v
@@ -232,7 +232,7 @@ Section lemmas.
   Lemma gmap_view_frag_valid k dq v : ✓ gmap_view_frag k dq v ↔ ✓ dq ∧ ✓ v.
   Proof.
     rewrite cmra_valid_validN. setoid_rewrite gmap_view_frag_validN.
-    rewrite cmra_valid_validN. naive_solver eauto using O.
+    rewrite [_ v]cmra_valid_validN. naive_solver eauto using O.
   Qed.
 
   Lemma gmap_view_frag_op k dq1 dq2 v1 v2 :
